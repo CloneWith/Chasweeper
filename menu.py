b4c100f42197c2f9bf55aff224194a499bb5f3ee
NaughtyChas <3030561972@qq.com>
2024-11-10 13:48:18 +0800

    Integrate statistics

--- menu.py
+++ menu.py
@@ -1,16 +1,20 @@
 import curses
-import diffcalc
+import os
 from board import Board
+from user import User
+from user_statistics import UserStatistics
 
 class Menu:
     def __init__(self, stdscr):
         self.stdscr = stdscr
         self.current_row = 0
         self.current_menu = "main"
+        self.current_user = None
+        self.users = self.load_users()
         self.menus = {
-            "main": ["Start Game", "View Statistics", "", "Exit Game"],
-            "start_game": ["Classic Mode", "Timed Mode", "", "Back"],
-            "classic_mode": ["Easy", "Hard", "Expert", "", "Back"]
+            "main": ["Start Game", "View Statistics", "Exit Game"],
+            "start_game": [],
+            "classic_mode": ["Easy", "Hard", "Expert", "Back"]
         }
         self.descriptions = {
             "Start Game": "* Play some Wordweeper!",
@@ -21,7 +25,9 @@ class Menu:
             "Back": "* Go back to the previous menu",
             "Easy": "* Easy difficulty",
             "Hard": "* Hard difficulty",
-            "Expert": "* Expert difficulty"
+            "Expert": "* Expert difficulty",
+            "New player? Click here to register!": "* Register a new player",
+            "Click here or press 'Enter' to register!": "* Register a new player"
         }
         self.ascii_art = [
             "  __          __           _                                   ",
@@ -33,6 +39,16 @@ class Menu:
             "                                              | |                ",
             "                                              |_|                "
         ]
+
+    def load_users(self):
+        users = []
+        if os.path.exists('./data/user.txt'):
+            with open('./data/user.txt', 'r') as file:
+                for line in file:
+                    data = line.strip().split(',')
+                    user = User(data[0], int(data[1]), int(data[2]), int(data[3]), data[4], int(data[5]), int(data[6]), int(data[7]), float(data[8]), int(data[9]), int(data[10]))
+                    users.append(user)
+        return users
 
     def check_window_size(self):
         h, w = self.stdscr.getmaxyx()
@@ -54,180 +70,108 @@ class Menu:
     def print_menu(self, menu):
         self.stdscr.clear()
         h, w = self.stdscr.getmaxyx()
-        
-        # Print ASCII art
-        for i, line in enumerate(self.ascii_art):
-            if len(line) > w:
-                line = line[:w-1]  # Truncate line if it's too long
-            self.stdscr.addstr(i, 0, line)
-        
         menu_start_y = (h - len(self.ascii_art) - len([item for item in menu if item != ""])) // 2 + len(self.ascii_art)
+        for idx, row in enumerate(self.ascii_art):
+            self.stdscr.addstr(idx, (w - len(row)) // 2, row)
         for idx, row in enumerate(menu):
-            x = 2  # Left-hand side with some padding
-            y = menu_start_y + idx
             if row == "":
-                self.stdscr.addstr(y, x, row)
                 continue  # Skip empty lines for spacing
+            x = 2  # Align menu items to the left
+            y = menu_start_y + idx
             if idx == self.current_row:
                 self.stdscr.attron(curses.color_pair(1))
                 self.stdscr.addstr(y, x, row)
                 self.stdscr.attroff(curses.color_pair(1))
             else:
                 self.stdscr.addstr(y, x, row)
-        
-        # Display description on the right-hand side
-        description = self.descriptions.get(menu[self.current_row], "")
-        description_x = w - len(description) - 2  # Right-hand side with some padding
-        description_y = menu_start_y
-        self.stdscr.addstr(description_y, description_x, description)
-        
         self.stdscr.refresh()
-
-    def run(self):
-        curses.curs_set(0)
-        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
-
-        main_menu = ["Start Game", "View Statistics", "Exit Game"]
-        start_game_menu = ["Classic Mode", "Timed Mode", "Back"]
-        classic_mode_menu = ["Easy", "Hard", "Expert", "Back"]
-
-        while True:
-            if not self.check_window_size():
-                continue
-
-            if self.current_menu == "main":
-                self.print_menu(main_menu)
-                menu = main_menu
-            elif self.current_menu == "start_game":
-                self.print_menu(start_game_menu)
-                menu = start_game_menu
-            elif self.current_menu == "classic_mode":
-                self.print_menu(classic_mode_menu)
-                menu = classic_mode_menu
-
-            key = self.stdscr.getch()
-
-            if key == curses.KEY_UP and self.current_row > 0:
-                self.current_row -= 1
-            elif key == curses.KEY_DOWN and self.current_row < len(menu) - 1:
-                self.current_row += 1
-            elif key == curses.KEY_ENTER or key in [10, 13]:
-                if menu[self.current_row] == "Start Game":
-                    diffcalc.update_words_file()  # Calculate word difficulties in words.txt
-                    self.current_menu = "start_game"
-                    self.current_row = 0
-                elif menu[self.current_row] == "Exit Game":
-                    exit()
-                elif self.current_menu == "start_game":
-                    if menu[self.current_row] == "Classic Mode":
-                        self.current_menu = "classic_mode"
-                        self.current_row = 0
-                    elif menu[self.current_row] == "Timed Mode":
-                        self.timed_mode()
-                    elif menu[self.current_row] == "Back":
-                        self.current_menu = "main"
-                        self.current_row = 0
-                elif self.current_menu == "classic_mode":
-                    if menu[self.current_row] == "Easy":
-                        self.start_game("Easy")
-                    elif menu[self.current_row] == "Hard":
-                        self.start_game("Hard")
-                    elif menu[self.current_row] == "Expert":
-                        self.start_game("Expert")
-                    elif menu[self.current_row] == "Back":
-                        self.current_menu = "start_game"
-                        self.current_row = 0
-            elif key == curses.KEY_MOUSE:
-                _, mx, my, _, button_state = curses.getmouse()
-                h, w = self.stdscr.getmaxyx()
-                menu_start_y = (h - len(self.ascii_art) - len([item for item in menu if item != ""])) // 2 + len(self.ascii_art)
-                for idx, row in enumerate(menu):
-                    if row == "":
-                        continue  # Skip empty lines for spacing
-                    y = menu_start_y + idx
-                    if y == my:
-                        self.current_row = idx
-                        if button_state & curses.BUTTON1_CLICKED:
-                            if row == "Exit Game":
-                                exit()
-                            elif row == "Back":
-                                if self.current_menu == "start_game":
-                                    self.current_menu = "main"
-                                elif self.current_menu == "classic_mode":
-                                    self.current_menu = "start_game"
-                                self.current_row = 0
-                            else:
-                                self.handle_enter()
-                        self.print_menu(menu)
-                        break
-            elif key == 27:  # ESC key
-                self.handle_esc()
-                self.print_menu(menu)
-
-    def move_up(self):
-        menu = self.menus[self.current_menu]
-        while True:
-            self.current_row = (self.current_row - 1) % len(menu)
-            if menu[self.current_row] != "":
-                break
-
-    def move_down(self):
-        menu = self.menus[self.current_menu]
-        while True:
-            self.current_row = (self.current_row + 1) % len(menu)
-            if menu[self.current_row] != "":
-                break
 
     def handle_enter(self):
         menu = self.menus[self.current_menu]
         if self.current_menu == "main":
             if menu[self.current_row] == "Start Game":
-                self.current_menu = "start_game"
-                self.current_row = 0
+                self.start_game()
             elif menu[self.current_row] == "View Statistics":
                 self.view_statistics()
             elif menu[self.current_row] == "Exit Game":
                 exit()
         elif self.current_menu == "start_game":
-            if menu[self.current_row] == "Classic Mode":
-                self.current_menu = "classic_mode"
+            if menu[self.current_row] == "New player? Click here to register!":
+                self.register()
+            else:
+                self.current_user = self.users[self.current_row]
+                self.current_menu = "user_menu"
                 self.current_row = 0
-            elif menu[self.current_row] == "Timed Mode":
-                self.timed_mode()
-            elif menu[self.current_row] == "Back":
-                self.current_menu = "main"
-                self.current_row = 0
-        elif self.current_menu == "classic_mode":
-            if menu[self.current_row] == "Easy":
-                self.start_game("Easy")
-            elif menu[self.current_row] == "Hard":
-                self.start_game("Hard")
-            elif menu[self.current_row] == "Expert":
-                self.start_game("Expert")
-            elif menu[self.current_row] == "Back":
-                self.current_menu = "start_game"
-                self.current_row = 0
+        elif self.current_menu == "register":
+            self.register()
 
-    def handle_esc(self):
-        if self.current_menu == "main":
-            exit()
+    def start_game(self):
+        if not self.users:
+            self.menus["start_game"] = ["Click here or press 'Enter' to register!"]
         else:
-            self.current_menu = "main"
-            self.current_row = 0
+            self.menus["start_game"] = [user.user_id for user in self.users] + ["New player? Click here to register!"]
+        self.current_menu = "start_game"
+        self.current_row = 0
 
-    def start_game(self, difficulty):
-        if difficulty == "Easy":
-            board = Board(self.stdscr, size=7)
-            board.run()
-
-    def timed_mode(self):
-        # timed mode logic
-        pass
+    def register(self):
+        self.stdscr.clear()
+        self.stdscr.addstr(0, 0, "Enter user ID: ")
+        curses.echo()
+        user_id = self.stdscr.getstr().decode('utf-8')
+        curses.noecho()
+        if User.load_from_file(user_id):
+            self.stdscr.addstr(1, 0, "User ID already exists. Press any key to continue.")
+            self.stdscr.getch()
+        else:
+            user = User(user_id)
+            user.save_to_file()
+            self.users.append(user)
+            self.stdscr.addstr(1, 0, "Registration successful. Press any key to continue.")
+            self.stdscr.getch()
+        self.current_menu = "main"
+        self.current_row = 0
 
     def view_statistics(self):
-        # view statistics logic
-        pass
+        stats = UserStatistics(self.stdscr, self.current_user)
+        stats.display()
+        self.current_menu = "main"
+        self.current_row = 0
+
+    def run(self):
+        curses.curs_set(0)
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
+        while True:
+            if not self.check_window_size():
+                continue
+            self.print_menu(self.menus[self.current_menu])
+            key = self.stdscr.getch()
+            if key == curses.KEY_UP and self.current_row > 0:
+                self.current_row -= 1
+            elif key == curses.KEY_DOWN and self.current_row < len(self.menus[self.current_menu]) - 1:
+                self.current_row += 1
+            elif key == curses.KEY_ENTER or key in [10, 13]:
+                self.handle_enter()
+            elif key == 27:  # ESC key
+                if self.current_menu == "main":
+                    exit()
+                elif self.current_menu == "start_game" or self.current_menu == "register":
+                    self.current_menu = "main"
+                    self.current_row = 0
+            elif key == curses.KEY_MOUSE:
+                _, mx, my, _, _ = curses.getmouse()
+                h, w = self.stdscr.getmaxyx()
+                menu_start_y = (h - len(self.ascii_art) - len([item for item in self.menus[self.current_menu] if item != ""])) // 2 + len(self.ascii_art)
+                for idx, row in enumerate(self.menus[self.current_menu]):
+                    if row == "":
+                        continue  # Skip empty lines for spacing
+                    x = 2  # Align menu items to the left
+                    y = menu_start_y + idx
+                    if y == my:
+                        self.current_row = idx
+                        self.handle_enter()
+                        break
 
 if __name__ == "__main__":
     curses.wrapper(Menu)
