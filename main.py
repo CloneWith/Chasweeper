d0d853a5350d21acd7d96edd4884fc8dc20ee258
NaughtyChas <3030561972@qq.com>
2024-11-12 10:57:59 +0800

    main.py comment

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from menu import Menu
 # Game launcher
 
 def main(stdscr):
+    # The main function that initializes and runs the menu for the puzzle game.
+    # Args:
+    #     stdscr: The standard screen object provided by the curses library.
     menu = Menu(stdscr)
     menu.run()
 
